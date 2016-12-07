2061afb41f292a6bc97d4fb4ec93d431d0c1587b
goodwill <serg.goodwill@gmail.com>
2016-12-07 23:30:50 +0200

    migrate to H2

--- src/main/java/org/igoodwill/jtutorsb/model/admin/Quest.java
+++ src/main/java/org/igoodwill/jtutorsb/model/admin/Quest.java
@@ -18,101 +18,101 @@ import javax.validation.constraints.Size;
 @Table(name = "quest")
 public class Quest {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-	@NotNull
-	@Size(min = 6, max = 32)
-	@Column(name = "quest_name", nullable = false, unique = true)
-	private String name;
+    @NotNull
+    @Size(min = 6, max = 32)
+    @Column(name = "quest_name", nullable = false, unique = true)
+    private String name;
 
-	@NotNull
-	@Size(min = 6, max = 64)
-	@Column(name = "quest_description", nullable = false)
-	private String description;
+    @NotNull
+    @Size(min = 6, max = 64)
+    @Column(name = "quest_description", nullable = false)
+    private String description;
 
-	@OneToMany(orphanRemoval = true)
-	@JoinColumn(name = "quest_id")
-	private List<Question> questions = new ArrayList<>();
+    @OneToMany(orphanRemoval = true)
+    @JoinColumn(name = "quest_id")
+    private List<Question> questions = new ArrayList<>();
 
-	public Integer getId() {
-		return id;
-	}
+    public Integer getId() {
+	return id;
+    }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public void setId(Integer id) {
+	this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+	return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+	return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+	this.description = description;
+    }
 
-	public List<Question> getQuestions() {
-		return questions;
-	}
+    public List<Question> getQuestions() {
+	return questions;
+    }
 
-	public void setQuestions(List<Question> questions) {
-		this.questions = questions;
-	}
+    public void setQuestions(List<Question> questions) {
+	this.questions = questions;
+    }
 
-	@Override
-	public String toString() {
-		return name;
-	}
+    @Override
+    public String toString() {
+	return name;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((questions == null) ? 0 : questions.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + ((description == null) ? 0 : description.hashCode());
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
+	result = prime * result + ((name == null) ? 0 : name.hashCode());
+	result = prime * result + ((questions == null) ? 0 : questions.hashCode());
+	return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Quest other = (Quest) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (questions == null) {
-			if (other.questions != null)
-				return false;
-		} else if (!questions.equals(other.questions))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Quest other = (Quest) obj;
+	if (description == null) {
+	    if (other.description != null)
+		return false;
+	} else if (!description.equals(other.description))
+	    return false;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
+	if (name == null) {
+	    if (other.name != null)
+		return false;
+	} else if (!name.equals(other.name))
+	    return false;
+	if (questions == null) {
+	    if (other.questions != null)
+		return false;
+	} else if (!questions.equals(other.questions))
+	    return false;
+	return true;
+    }
 }
