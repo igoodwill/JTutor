e44f70003d51057845891b56e43c763d643949c6
Igor <igor.goodwill1@gmail.com>
2016-12-09 13:17:44 +0200

    removed h2 tails

--- src/main/java/org/igoodwill/jtutorsb/configuration/JavaConfiguration.java
+++ src/main/java/org/igoodwill/jtutorsb/configuration/JavaConfiguration.java
@@ -2,22 +2,14 @@ package org.igoodwill.jtutorsb.configuration;
 
 import javax.sql.DataSource;
 
-import org.h2.server.web.WebServlet;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class JavaConfiguration {
-	@Bean
-	ServletRegistrationBean h2servletRegistration() {
-		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
-		registrationBean.addUrlMappings("/console/*");
-		return registrationBean;
-	}
 
 	@Bean
 	@Primary
