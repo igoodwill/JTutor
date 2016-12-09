8c066f02278d83f2d1d621e968dca8d6f66159c9
Igor <igor.goodwill1@gmail.com>
2016-12-09 23:04:45 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/JTutorSbApplication.java
+++ src/main/java/org/igoodwill/jtutorsb/JTutorSbApplication.java
@@ -5,28 +5,27 @@ import java.util.Locale;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class JTutorSbApplication extends WebMvcConfigurerAdapter {
+public class JTutorSbApplication extends SpringBootServletInitializer {
 
 	public static Logger log = Logger.getLogger(JTutorSbApplication.class.getName());
 
-	/*
-	 * @Override protected SpringApplicationBuilder
-	 * configure(SpringApplicationBuilder application) { return
-	 * application.sources(JTutorSbApplication.class); }
-	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JTutorSbApplication.class);
+	}
 
 	@Bean
 	public LocaleResolver localeResolver() {
 		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.US);
+		slr.setDefaultLocale(Locale.ENGLISH);
 		return slr;
 	}
 
@@ -40,8 +39,4 @@ public class JTutorSbApplication extends WebMvcConfigurerAdapter {
 	public static void main(String[] args) {
 		SpringApplication.run(JTutorSbApplication.class, args);
 	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeChangeInterceptor());
 }
