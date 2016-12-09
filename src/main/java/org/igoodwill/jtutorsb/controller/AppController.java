e3d89ab857c87c699338e5fd75e6b24989ebc2cb
Igor <igor.goodwill1@gmail.com>
2016-12-09 22:37:40 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/controller/AppController.java
+++ src/main/java/org/igoodwill/jtutorsb/controller/AppController.java
@@ -1,42 +1,22 @@
 package org.igoodwill.jtutorsb.controller;
 
 import java.util.Date;
-import java.util.Locale;
 import java.util.Map;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.igoodwill.jtutorsb.model.admin.Users;
 import org.igoodwill.jtutorsb.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
 
 @Controller
 public class AppController {
-
-	public void process(final HttpServletRequest request, final HttpServletResponse response,
-			final ServletContext servletContext, final TemplateEngine templateEngine) throws Exception {
-
-		WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-
-		templateEngine.process("label", ctx, response.getWriter());
-	}
 
 	@Autowired
 	private UsersService usersService;
 
 	@GetMapping({ "/", "/home" })
-	public String homePage(final HttpServletRequest request, final HttpServletResponse response,
-			final ServletContext servletContext, final TemplateEngine templateEngine, Locale locale,
-			Map<String, Object> model) throws Exception {
-		WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-
-		templateEngine.process("label", ctx, response.getWriter());
+	public String homePage(Map<String, Object> model) {
 		Users loggedInUser = usersService.getLoggedInUser();
 		model.put("isAdminOrTutor", loggedInUser.isAdmin() || loggedInUser.isTutor());
 		model.put("date", new Date());
