df5c691385e306fdb6856e6d71f76e4d406ca15e
Igor <igor.goodwill1@gmail.com>
2016-12-09 23:09:33 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/JTutorSbApplication.java
+++ src/main/java/org/igoodwill/jtutorsb/JTutorSbApplication.java
@@ -1,16 +1,10 @@
 package org.igoodwill.jtutorsb;
 
-import java.util.Locale;
-
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class JTutorSbApplication extends SpringBootServletInitializer {
@@ -20,20 +14,6 @@ public class JTutorSbApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(JTutorSbApplication.class);
-	}
-
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.ENGLISH);
-		return slr;
-	}
-
-	@Bean
-	public LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		return lci;
 	}
 
 	public static void main(String[] args) {
