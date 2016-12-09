d63e8e92b38d52025dbbc2bb4805e9a36834dedd
Igor <igor.goodwill1@gmail.com>
2016-12-09 22:59:33 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/JTutorSbApplication.java
+++ src/main/java/org/igoodwill/jtutorsb/JTutorSbApplication.java
@@ -5,22 +5,23 @@ import java.util.Locale;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
-public class JTutorSbApplication extends SpringBootServletInitializer {
+public class JTutorSbApplication extends WebMvcConfigurerAdapter {
 
 	public static Logger log = Logger.getLogger(JTutorSbApplication.class.getName());
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(JTutorSbApplication.class);
-	}
+	/*
+	 * @Override protected SpringApplicationBuilder
+	 * configure(SpringApplicationBuilder application) { return
+	 * application.sources(JTutorSbApplication.class); }
+	 */
 
 	@Bean
 	public LocaleResolver localeResolver() {
@@ -39,4 +40,8 @@ public class JTutorSbApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(JTutorSbApplication.class, args);
 	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(localeChangeInterceptor());
 }
