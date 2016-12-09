d1d80a4c9c3774f58c8e139cbd773f80ce5dd585
Igor <igor.goodwill1@gmail.com>
2016-12-09 22:20:27 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/controller/AppController.java
+++ src/main/java/org/igoodwill/jtutorsb/controller/AppController.java
@@ -3,14 +3,29 @@ package org.igoodwill.jtutorsb.controller;
 import java.util.Date;
 import java.util.Map;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.igoodwill.jtutorsb.model.admin.Users;
 import org.igoodwill.jtutorsb.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 @Controller
 public class AppController {
+
+	public void process(final HttpServletRequest request, final HttpServletResponse response,
+			final ServletContext servletContext, final TemplateEngine templateEngine) throws Exception {
+
+		WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+
+		templateEngine.process("label", ctx, response.getWriter());
+
+	}
 
 	@Autowired
 	private UsersService usersService;
