b360517969c11430cb92f66a4a08815e656cbe11
Igor <igor.goodwill1@gmail.com>
2016-12-10 10:52:22 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/controller/LectureController.java
+++ src/main/java/org/igoodwill/jtutorsb/controller/LectureController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -30,8 +29,8 @@ public class LectureController {
 	}
 
 	@GetMapping("/tutorial")
-	public String showLecture(final Model model, @RequestBody final Integer id) {
-		Lecture lecture = lectureRepo.findOne(id);
+	public String showLecture(final Model model, @RequestParam("id") final Integer lectureId) {
+		Lecture lecture = lectureRepo.findOne(lectureId);
 
 		if (lecture == null) {
 			return "redirect:/home";
@@ -40,6 +39,11 @@ public class LectureController {
 		model.addAttribute("lecture", lecture);
 
 		return "lecture/show";
+	}
+
+	@GetMapping("/tutorial")
+	public String redirectToHome() {
+		return "redirect:/home";
 	}
 
 	// ADMIN
