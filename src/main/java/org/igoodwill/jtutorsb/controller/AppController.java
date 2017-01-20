952705fdd6d84e4588dec5e462a904276d862655
igoodwill <igor.goodwill1@gmail.com>
2017-01-20 17:59:52 +0200

    Acmp

--- src/main/java/org/igoodwill/jtutorsb/controller/AppController.java
+++ src/main/java/org/igoodwill/jtutorsb/controller/AppController.java
@@ -4,11 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 @Controller
 public class AppController {
 
 	@GetMapping({ "/", "/home" })
 	public String homePage(final Model model) {
+		
+		ServerSocket server = new ServerSocket(8953);
+        	Socket socket = server.accept();
+
+        	DataInputStream input = new DataInputStream(socket.getInputStream());
+        	System.out.println(input.readUTF());
+
+        	socket.close();
+        	server.close();
+		
 		return "index";
 	}
 }
