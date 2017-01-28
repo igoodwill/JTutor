89da9afe3772ac448b6d615954e935a3f2d99134
Igor <igor.goodwill1@gmail.com>
2017-01-28 12:06:01 +0200

    update

--- src/main/java/org/igoodwill/jtutorsb/model/UserAnswer.java
+++ src/main/java/org/igoodwill/jtutorsb/model/UserAnswer.java
@@ -14,79 +14,79 @@ import org.igoodwill.jtutorsb.model.admin.Answer;
 @Table(name = "user_answer")
 public class UserAnswer {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @NotNull
-    @Size(min = 1)
-    private String value;
+	@NotNull
+	@Size(min = 1)
+	private String value;
 
-    @NotNull
-    private boolean state;
+	@NotNull
+	private boolean state;
 
-    public UserAnswer() {
-    }
+	public UserAnswer() {
+	}
 
-    public UserAnswer(Answer answer) {
-	this.value = answer.getValue();
-    }
+	public UserAnswer(Answer answer) {
+		this.value = answer.getValue();
+	}
 
-    public Integer getId() {
-	return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-	this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getValue() {
-	return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setValue(String value) {
-	this.value = value;
-    }
+	public void setValue(String value) {
+		this.value = value;
+	}
 
-    public boolean isState() {
-	return state;
-    }
+	public boolean isState() {
+		return state;
+	}
 
-    public void setState(boolean state) {
-	this.state = state;
-    }
+	public void setState(boolean state) {
+		this.state = state;
+	}
 
-    @Override
-    public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + ((id == null) ? 0 : id.hashCode());
-	result = prime * result + (state ? 1231 : 1237);
-	result = prime * result + ((value == null) ? 0 : value.hashCode());
-	return result;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + (state ? 1231 : 1237);
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-	if (this == obj)
-	    return true;
-	if (obj == null)
-	    return false;
-	if (getClass() != obj.getClass())
-	    return false;
-	UserAnswer other = (UserAnswer) obj;
-	if (id == null) {
-	    if (other.id != null)
-		return false;
-	} else if (!id.equals(other.id))
-	    return false;
-	if (state != other.state)
-	    return false;
-	if (value == null) {
-	    if (other.value != null)
-		return false;
-	} else if (!value.equals(other.value))
-	    return false;
-	return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserAnswer other = (UserAnswer) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (state != other.state)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
